9d8bc0f84e6015164555ac6d9c5fc59a54c39eb3
Johannes Millan <johannes.millan@gmail.com>
2017-08-17 11:07:11 +0200

    add basic prompt

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const low = require('lowdb');
 const db = low('db.json');
+const prompt = require('prompt');
 
-// Set some defaults if your JSON file is empty
-db.defaults({
-  message: '',
-  time: null
-})
-  .write();
+class App {
+  constructor() {
+  }
+
+  initDb() {
+    // Set some defaults if your JSON file is empty
+    db.defaults({
+      message: '',
+      time: null
+    })
+      .write();
+  }
+
+  startPrompt() {
+    prompt.start();
+
+    console.log('Please enter time when you want be leave the computer.');
+    prompt.get(['time'], (err, result) => {
+      console.log('  time: ' + result.time);
+    });
+  }
+}
+
+const app = new App();
+app.initDb();
+app.startPrompt();
+
