ff882a5c8a99609911e1d0b5a2006fba963b6e12
Johannes Millan <johannes.millan@gmail.com>
2017-08-17 12:14:49 +0200

    refactor background process

--- background-process.js
+++ background-process.js
@@ -2,33 +2,27 @@ const exec = require('child_process').exec;
 const Store = require('jfs');
 const db = new Store('db.json', { pretty: true });
 
-
 function sendMsg(title, msg, icon) {
-  console.log(title, msg, icon)
-  let sCommand = 'notify-send';
+  let command = 'notify-send';
 
-  if (icon && icon !== "") {
-    sCommand += ' -i ' + icon;
+  if (icon && icon !== '') {
+    command += ' -i ' + icon;
   }
   if (title) {
-    sCommand += ' "' + title + ' "';
+    command += ' "' + title + ' "';
   } else {
-    sCommand += ' "' + ' "';
+    command += ' "' + ' "';
   }
   if (msg && msg !== "") {
-    sCommand += ' "' + msg + '"';
+    command += ' "' + msg + '"';
   }
-  console.log(sCommand);
-
-  exec(sCommand);
-
+  exec(command);
 }
 
 const time = db.getSync('time');
 const msg = db.getSync('message');
 const icon = db.getSync('icon');
+const repetitions = db.getSync('repetitions');
+const repetitionInterval = db.getSync('repetitionInterval');
 
-console.log(time, msg, icon);
-
-//sendMsg(msg, time);
-sendMsg('msg', 'time');
+sendMsg(msg, null, icon);
