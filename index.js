b9d433a17792e0598cd39a291f5915ec0f5b9b81
Johannes Millan <johannes.millan@gmail.com>
2017-08-17 11:39:38 +0200

    add moment.js and a nice goodbye message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const low = require('lowdb');
 const db = low('db.json');
 const prompt = require('prompt');
+const moment = require('moment');
 
 class App {
   constructor() {
@@ -20,8 +21,16 @@ class App {
 
     console.log('Please enter time when you want be leave the computer.');
     prompt.get(['time'], (err, result) => {
-      console.log('  time: ' + result.time);
+      this.time = moment(result.time, 'H:m');
+
+      const now = moment();
+      const timeDiff = this.getTimeDifference(now, this.time);
+      console.log(`You will be notified in ${timeDiff} at ${this.time.format('HH:mm')} o'clock.`);
     });
+  }
+
+  getTimeDifference(now, then) {
+    return moment.duration(now.diff(then)).humanize();
   }
 }
 
