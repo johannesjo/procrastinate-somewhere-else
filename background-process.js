d40f5d5c779fe0be4966a45aaf5b6c081e8049f5
Johannes Millan <johannes.millan@gmail.com>
2017-08-17 12:41:56 +0200

    add proper scheduling of jobs

--- background-process.js
+++ background-process.js
@@ -1,5 +1,7 @@
+const moment = require('moment');
 const exec = require('child_process').exec;
 const Store = require('jfs');
+const schedule = require('node-schedule');
 const db = new Store('db.json', { pretty: true });
 
 function sendMsg(title, msg, icon) {
@@ -19,10 +21,46 @@ function sendMsg(title, msg, icon) {
   exec(command);
 }
 
-const time = db.getSync('time');
-const msg = db.getSync('message');
-const icon = db.getSync('icon');
-const repetitions = db.getSync('repetitions');
-const repetitionInterval = db.getSync('repetitionInterval');
+function launchAction(action) {
+  if (action === 'LOCK') {
+    exec('gnome-screensaver-command -l');
+  } else if (action === 'SUSPEND') {
+    exec('systemctl suspend');
+  } else if (action === 'SHUTDOWN') {
+    exec('systemctl poweroff');
+  }
+}
 
-sendMsg(msg, null, icon);
+function scheduleJob() {
+  const time = moment(db.getSync('time')).toDate();
+  schedule.scheduleJob(time, () => {
+    const msg = db.getSync('message');
+    const icon = db.getSync('icon');
+    const repetitions = db.getSync('repetitions');
+    const repetitionInterval = db.getSync('repetitionInterval');
+    const action = db.getSync('action');
+    const timeOutBeforeLock = db.getSync('timeOutBeforeLock');
+
+    let currentRepetitions = 0;
+
+    // send one right away
+    sendMsg(msg, null, icon);
+
+    // one timeout should be enough as we don't need any reps then
+    setTimeout(() => {
+      launchAction(action)
+    }, timeOutBeforeLock);
+
+    const timerId = setInterval(() => {
+
+      currentRepetitions++;
+      sendMsg(msg, null, icon);
+      if (currentRepetitions === repetitions) {
+        clearInterval(timerId);
+      }
+    }, repetitionInterval);
+
+  });
+}
+
+scheduleJob();
